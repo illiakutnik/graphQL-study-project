431355e40097a369079e458bb149c48e2065bc22
Illia KUtnik <illiakutnik@gmail.com>
2020-02-25 20:29:05 +0200

    first commit

---
+++ server/app.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const graphqlHTTP = require('express-graphql')
+const mongoose = require('mongoose')
+
+const schema = require('./schema/schema')
+
+const db =
+	'mongodb+srv://illiakutnik:bagdad1258@cluster0-ztp3p.mongodb.net/test?retryWrites=true&w=majority'
+
+const app = express()
+
+const connectDB = async () => {
+	try {
+		await mongoose.connect(db, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true
+			// useCreateIndex: true,
+			// useFindAndModify: false
+		})
+		console.log('mongo connected')
+	} catch (err) {
+		console.error(err.message)
+		process.exit(1)
+	}
+}
+
+connectDB()
+
+app.use(
+	'/graphql',
+	graphqlHTTP({
+		schema,
+		graphiql: true
+	})
+)
+
+app.listen(4001, () => {
+	console.log('listening on port 4001')
+})
